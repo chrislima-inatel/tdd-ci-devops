3df888264d34ad9e5cd3e11e75df307209e5ab18
Chris Lima <christopher@inatel.br>
2024-04-03 14:19:30 -0300

    enviando o arquivo de scripts

--- scripts/shell.sh
+++ scripts/shell.sh
@@ -1,3 +1,10 @@
 echo "HAHAHAHAHAHAH"
 ls
 pwd
+git version
+sudo apt install neofetch
+neofetch
+
+sudo apt-get install mailutils
+echo "Fim da instalacao"
+echo "Mandando e-mail com mail do linux - aula C214" | mail -s "a subject" christopher@inatel.br
